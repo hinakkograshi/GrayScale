13edc7eead027c0ced2b3794c25965472984f6b9
hinakkograshi <hinaf0224@gmail.com>
2025-11-23 13:52:00 +0900

    update fast wasm

--- Sources/GrayScale/main.swift
+++ Sources/GrayScale/main.swift
@@ -7,24 +7,25 @@ JSObject.global["convertBySwift"] = .object(
         let dataJS = args[0].object!
         let lengthJS = args[1].number!
         let typedArray = JSUInt8ClampedArray(unsafelyWrapping: dataJS)
-        var data: [UInt8] = typedArray.withUnsafeBytes { Array($0) }
-        let convertedData = makeGrascale(data: data)
-        
-        print("converting by Swift")
-        return JSUInt8ClampedArray(convertedData).jsValue
+        return typedArray.withUnsafeBytes { makeGrascale(data: $0) }.jsValue
     }
 )
 
-func makeGrascale(data:[UInt8]) -> [UInt8] {
-    var grayData = data
-    for i in stride(from: 0, to: data.count, by: 4) {
-        let r = Float(data[i])
-        let g = Float(data[i + 1])
-        let b = Float(data[i + 2])
-        let gray = UInt8(0.299 * r + 0.587 * g + 0.114 * b)
-        grayData[i] = gray
-        grayData[i + 1] = gray
-        grayData[i + 2] = gray
+func makeGrascale(data: UnsafeBufferPointer<UInt8>) -> JSUInt8ClampedArray {
+    let out = UnsafeMutableBufferPointer<UInt8>.allocate(capacity: data.count)
+    defer {
+        out.deallocate()
     }
-    return grayData
+    do {
+        for i in stride(from: 0, to: data.count, by: 4) {
+            let r = Float(data[i])
+            let g = Float(data[i + 1])
+            let b = Float(data[i + 2])
+            let gray = UInt8(0.299 * r + 0.587 * g + 0.114 * b)
+            out[i] = gray
+            out[i + 1] = gray
+            out[i + 2] = gray
+        }
+    }
+    return JSUInt8ClampedArray(buffer: UnsafeBufferPointer(out))
 }
