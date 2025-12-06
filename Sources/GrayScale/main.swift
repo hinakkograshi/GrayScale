8e426243d096e8b647336583ace059fd1e4627e1
hinakko <131275914+hinakko02@users.noreply.github.com>
2025-12-07 02:01:57 +0900

    update refactor

--- Sources/GrayScale/main.swift
+++ Sources/GrayScale/main.swift
@@ -1,11 +1,9 @@
 import JavaScriptKit
 
-let document = JSObject.global.document
-
 JSObject.global["convertBySwift"] = .object(
     JSClosure { args in
-        let dataJS = args[0].object!
-        let typedArray = JSUInt8ClampedArray(unsafelyWrapping: dataJS)
+        let imageDataJS = args[0].object!
+        let typedArray = JSUInt8ClampedArray(unsafelyWrapping: imageDataJS)
         return typedArray.withUnsafeBytes { makeGrascale(data: $0) }.jsValue
     }
 )
@@ -14,19 +12,13 @@ func makeGrascale(data: UnsafeBufferPointer<UInt8>) -> JSUInt8ClampedArray {
     let count = data.count
     // 新しいバッファをアロケートし、元のデータで初期化（コピー）
     let out = UnsafeMutableBufferPointer<UInt8>.allocate(capacity: count)
-    // 画像のアルファチャンネル（透明度）が適切に保持されるようになった
-    // i+3 の位置（アルファ値）への書き込みがなかったため、out[i + 3]の値は未初期化のまま.
-    // 0 に近かった場合、透明に見えてしまっていた
     _ = out.initialize(from: data) // <- 初期化とコピーを同時に実行
 
     defer {
-        // out.deallocate() の代わりに、JSUInt8ClampedArrayの生成時に渡すことで、
-        // Swiftのメモリ管理の外に出す（Wasmではこの部分の扱いに注意が必要）
-        // ただし、この関数内で完結させるなら deallocate は必要
         out.deallocate()
     }
 
-    // 2. グレースケール計算のみを実行
+    // グレースケール計算を実行
     for i in stride(from: 0, to: count, by: 4) {
         let r = Float(out[i])
         let g = Float(out[i + 1])
@@ -38,9 +30,8 @@ func makeGrascale(data: UnsafeBufferPointer<UInt8>) -> JSUInt8ClampedArray {
         out[i] = grayValue     // Rを上書き
         out[i + 1] = grayValue // Gを上書き
         out[i + 2] = grayValue // Bを上書き
-        // out[i + 3] (アルファ値) は変更しない
     }
     
-    // 3. 結果を返す
+    // 結果を返す
     return JSUInt8ClampedArray(buffer: UnsafeBufferPointer(out))
 }
